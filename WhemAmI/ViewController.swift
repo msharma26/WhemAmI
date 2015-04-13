bb5376758989c8260552019a9f7bf01fcea7bea0
RShankar <ravishankar@outlook.in>
2015-04-13 18:30:57 +0530

    Fixed for iOS 8.3
    Fixed for iOS 8.3

--- WhemAmI/ViewController.swift
+++ WhemAmI/ViewController.swift
@@ -34,7 +34,7 @@ class ViewController: UIViewController, CLLocationManagerDelegate {
             }
             
             if placemarks.count > 0 {
-               let pm = placemarks[0] as CLPlacemark
+               let pm = placemarks[0] as! CLPlacemark
                self.displayLocationInfo(pm)
             } else {
                 println("Problem with the data received from geocoder")
